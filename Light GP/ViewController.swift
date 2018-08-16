3bf96c8cf918711ce9f1869e49c8d233a610e5c4
eryshkov <wendor265>
2018-08-16 12:25:05 +0300

    Initial Commit

---
+++ Light GP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Light GP
+//
+//  Created by Evgeniy Ryshkov on 16.08.2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
